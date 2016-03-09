68e8a8250e567dea89a043379f0f137f6ddbf390
chris watts <chris@byhook.com>
2016-03-08 17:11:40 -0800

    working on quartile tracking events

--- budo/app.js
+++ budo/app.js
@@ -52,12 +52,44 @@ App.prototype._ytloaded = function () {
 
   console.log ("YT api loaded");
 
-  var player = new YTPlayerWrapper.Player ();
+  var el = document.querySelector('#ad-root');
+    var params = {
+      height: '250',
+      width: '970',
+      videoId: "zRa3X1IqcgI",
+      playerVars: {
 
+        end: 25,
+        start: 0,
+        rel: 0,
+        showinfo: 0,
+        enablejsapi: 1,
+        disablekb: 1,
+        iv_load_policy: 3,
+        cc_load_policy: 0,
+        adformat: '1_8',
+        controls: 1,
+        html5: 1,
+        origin: document.domain,
+        fs: 0
+      }
+    };
+
+  this.player = new YTPlayerWrapper.Player ();
+
+  this.player.playerReadySignal.addOnce (this._playerReadyHandler,this);
+
+  this.player.load (el,params);
 
 
 };
 
+
+App.prototype._playerReadyHandler = function () {
+
+this.player.player.playVideo();
+
+};
 
 
 
