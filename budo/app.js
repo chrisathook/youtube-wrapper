08e86e010ef0d187350cedfd6357aff6bec77738
chris watts <chris@byhook.com>
2016-04-26 12:05:43 -0700

    updating index file

--- budo/app.js
+++ budo/app.js
@@ -1,21 +1,14 @@
-
 "use strict";
 
-
-
-var YTPlayerWrapper = require ("../index.js").YTPlayerWrapper;
-var TrackingManifest = require ("../index.js").TrackingManifest;
-
+var YTPlayerWrapper = require("../index.js").YTPlayerWrapper;
+var TrackingManifest = require("../index.js").TrackingManifest;
+var YTApiLoader = require("../index.js").YTApiLoader;
 
 var App = function () {
 
-
   this.loadDC();
 
-
-
 };
-
 
 App.prototype.loadDC = function () {
 
@@ -25,74 +18,58 @@ App.prototype.loadDC = function () {
     this.enablerInitialized();
   }
 
-
 };
-
-
 
 App.prototype.enablerInitialized = function () {
 
   console.log("DC Ready");
 
+  var ytLoader = YTApiLoader.getInstance();
 
-  var tag = document.createElement('script');
-  tag.src = 'https://www.youtube.com/iframe_api';
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  window.onYouTubeIframeAPIReady= this._ytloaded.bind (this);
-
-
-
+  ytLoader.apiReady.addOnce(this._ytloaded, this);
+  ytLoader.load ();
 
 };
 
-
 App.prototype._ytloaded = function () {
-
 
   console.log ("YT api loaded");
 
   var el = document.querySelector('#ad-root');
-    var params = {
-      height: '250',
-      width: '970',
-      videoId: "zRa3X1IqcgI",
-      playerVars: {
+  var params = {
+    height: '250',
+    width: '970',
+    videoId: "zRa3X1IqcgI",
+    playerVars: {
 
-        end: 25,
-        start: 0,
-        rel: 0,
-        showinfo: 0,
-        enablejsapi: 1,
-        disablekb: 1,
-        iv_load_policy: 3,
-        cc_load_policy: 0,
-        adformat: '1_8',
-        controls: 1,
-        html5: 1,
-        origin: document.domain,
-        fs: 0
-      }
-    };
+      end: 25,
+      start: 0,
+      rel: 0,
+      showinfo: 0,
+      enablejsapi: 1,
+      disablekb: 1,
+      iv_load_policy: 3,
+      cc_load_policy: 0,
+      adformat: '1_8',
+      controls: 1,
+      html5: 1,
+      origin: document.domain,
+      fs: 0
+    }
+  };
 
-  this.player = new YTPlayerWrapper (TrackingManifest.DEFAULT);
+  this.player = new YTPlayerWrapper(TrackingManifest.DEFAULT);
 
-  this.player.readySignal.addOnce (this._playerReadyHandler,this);
+  this.player.readySignal.addOnce (this._playerReadyHandler, this);
 
-  this.player.load (el,params);
-
+  this.player.load (el, params);
 
 };
-
 
 App.prototype._playerReadyHandler = function (signal) {
 
   this.player.ytPlayer.playVideo();
 
 };
-
-
-
 
 module.exports = App;
