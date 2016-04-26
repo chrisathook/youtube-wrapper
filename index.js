886af70e273565f341a78b5eac18acdc6b051305
chris watts <chris@byhook.com>
2016-04-26 11:58:29 -0700

    merging edits

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 
 module.exports = {
+  YTApiLoader:  require('./lib/YTApiLoader'),
   YTPlayerWrapper:  require('./lib/YTPlayerWrapper'),
   TrackingManifest: require('./lib/TrackingManifest')
 };
